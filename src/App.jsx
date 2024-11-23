41e155ee22a0f3a23acfc9e46d16e7a794324e5f
jobserd12 <114456323+Jobserd12@users.noreply.github.com>
2024-11-22 22:37:25 -0500

    feat: add new API routes, consolidating existing routes into a single file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Index from "./views/core/Index";
 import Detail from "./views/core/Detail";
 import Search from "./views/core/Search";
 import Category from "./views/core/Category";
-import About from "./views/pages/About";
 import Contact from "./views/pages/Contact";
 import Register from "./views/auth/Register";
 import Login from "./views/auth/Login";
 import Logout from "./views/auth/Logout";
 import ForgotPassword from "./views/auth/ForgotPassword";
 import CreatePassword from "./views/auth/CreatePassword";
-import Dashboard from "./views/dashboard/Dashboard";
-import Posts from "./views/dashboard/Posts";
-import AddPost from "./views/dashboard/AddPost";
-import EditPost from "./views/dashboard/EditPost";
-import Comments from "./views/dashboard/Comments";
-import Notifications from "./views/dashboard/Notifications";
+import Posts from "./views/admin/Posts";
+import CreatePost from "./views/admin/CreatePost";
+import EditPost from "./views/admin/EditPost";
+import Comments from "./views/admin/Comments";
+import Notifications from "./views/admin/Notifications";
 import ErrorBoundary from "./utils/ErrorBoundary ";
 import MainWrapper from "./layouts/MainWrapper";
 import PrivateRoute from "./layouts/PrivateRoute";
 import PublicRoute from "./layouts/PublicRoute";
 import ErrorPage from "./views/pages/ErrorPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import LIbrary from "./views/dashboard/LIbrary";
-import Stats from "./views/dashboard/Stats";
+import Stats from "./views/admin/Stats";
 import NotFound from "./views/pages/NotFound";
 import ProfilePage from "./views/pages/ProfilePage";
-import Editor from "./views/pages/Editor";
+import HelpCenter from "./views/pages/Help";
+import AccountSettings from "./views/pages/AccountSettings";
 
 const queryClient = new QueryClient();
 
@@ -57,26 +54,23 @@ function App() {
 
                 {/* Dashboard */}
                 <Route element={<PrivateRoute />}>
-                  <Route path="/library" element={<LIbrary />} />
                   <Route path="/stats" element={<Stats />} />
                   <Route path="/logout" element={<Logout />} />
 
-                  
-                  <Route path="/write" element={<Editor />} />
-
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/stories" element={<Posts />} />
-                  <Route path="/add-post" element={<AddPost />} />
+                  <Route path="/post" element={<Posts />} />
+                  <Route path="/new-post" element={<CreatePost />} />
                   <Route path="/edit-post/:id" element={<EditPost />} />
+
                   <Route path="/comments" element={<Comments />} />
                   <Route path="/notifications" element={<Notifications />} />
                 </Route>
 
                 {/* Pages */}
-                <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/settings" element={<AccountSettings />} />
+                <Route path="/help" element={<HelpCenter />} />
 
-                <Route path="*" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} /> 
               </Routes>
             </MainWrapper>
           </ErrorBoundary>
